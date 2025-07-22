9ff0e8e40e6aaa3861c61d48a96bcae87fc7cbec
felmunpen <felmunpen@gmail.com>
2025-07-22 10:45:20 +0200

    Changes from github desktop

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
 print("How is the weather today in your city? It's colder in here than Pingu's Communion.")
 print("Whe should add some answers here.")
 print("To the team: whe should add some answers here.")
+print("I have another questions for you.")
